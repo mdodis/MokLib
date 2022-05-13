100a4a59da5d80bd3a8101404342a5621fc9d98f
mdodis <michaeldodisgr@gmail.com>
2022-05-13 10:36:34 +0300

    remove old write_file() overload with StringBuilder in fs extras

--- FileSystem/Extras.h
+++ FileSystem/Extras.h
@@ -1,7 +1,7 @@
 #pragma once
 #include "FileSystem.h"
 #include "Memory/Base.h"
-#include "StringBuilder.h"
+#include "Containers/List.h"
 
 static _inline bool write_file(FileHandle &handle, TList<Str> what) {
     for (const Str &str : what) {
@@ -12,10 +12,6 @@ static _inline bool write_file(FileHandle &handle, TList<Str> what) {
         }
     }
     return true;
-}
-
-static _inline bool write_file(FileHandle &handle, StringBuilder builder) {
-    return write_file(handle, builder.to_list());
 }
 
 static _inline bool write_file(FileHandle &handle, const Str &str) {
