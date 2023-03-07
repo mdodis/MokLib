4932d340b1eb2a522a547ce128e29370e416027c
Michael Dodis <michaeldodisgr@gmail.com>
2023-03-07 08:16:55 +0200

    fix dump_file not closing file after dumping it, reformat Filesystem/Extras.h

--- FileSystem/Extras.h
+++ FileSystem/Extras.h
@@ -1,59 +1,63 @@
 #pragma once
+#include "Containers/List.h"
 #include "FileSystem.h"
 #include "Memory/Base.h"
-#include "Containers/List.h"
 #include "StringFormat.h"
 
-static _inline bool write_file(FileHandle &handle, TList<Str> what) {
-    for (const Str &str : what) {
-
+static _inline bool write_file(FileHandle& handle, TList<Str> what)
+{
+    for (const Str& str : what) {
         u64 bytes_written;
-        if (!write_file(handle, str.data, str.len, &bytes_written)
-            || (bytes_written != str.len)) {
+        if (!write_file(handle, str.data, str.len, &bytes_written) ||
+            (bytes_written != str.len))
+        {
             return false;
         }
     }
     return true;
 }
 
-static _inline bool write_file(FileHandle &handle, const Str &str) {
-    u64 bytes_written;
+static _inline bool write_file(FileHandle& handle, const Str& str)
+{
+    u64  bytes_written;
     bool result = write_file(handle, str.data, str.len, &bytes_written);
 
     return (!result) || (bytes_written != str.len);
 }
 
-static _inline Raw dump_file(FileHandle &handle, IAllocator &alloc) {
+static _inline Raw dump_file(FileHandle& handle, IAllocator& alloc)
+{
     u64 file_size = get_file_size(handle);
 
     umm file_data = alloc.reserve(file_size);
-    if (!file_data)
-        return Raw{0};
+    if (!file_data) return Raw{0};
 
     if (read_file(handle, file_data, file_size) != file_size) {
         alloc.release(file_data);
         return Raw{0};
     }
 
-    return Raw { file_data, file_size };
+    return Raw{file_data, file_size};
 }
 
 static _inline Raw dump_file(
-    Str filename,
-    IAllocator &alloc = *get_system_allocator())
+    Str filename, IAllocator& alloc = *get_system_allocator())
 {
     FileHandle handle = open_file(filename, FileMode::Read);
-    return dump_file(handle, alloc);
+    Raw        result = dump_file(handle, alloc);
+    close_file(handle);
+    return result;
 }
 
-
-static _inline void print(Str fmt_str) {
+static _inline void print(Str fmt_str)
+{
     auto tape = get_stream(Console::Output);
     tape.write_str(fmt_str);
 }
 
 template <typename First, typename... Rest>
-static _inline void print(Str fmt_str, const First &first, const Rest&... rest) {
+static _inline void print(Str fmt_str, const First& first, const Rest&... rest)
+{
     auto tape = get_stream(Console::Output);
     format(&tape, fmt_str, first, rest...);
 }
