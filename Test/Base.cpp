febdb06c08a706ffc740ba153d3b8a59dcbb8a49
Michael Dodis <michaeldodisgr@gmail.com>
2023-03-08 09:48:28 +0200

    refactor Test/Base.cpp

--- Test/Base.cpp
+++ Test/Base.cpp
@@ -1,36 +1,32 @@
 #include "Base.h"
-#include "Log.h"
-#include "Console.h"
-#include "Str.h"
-#include "FileSystem/Extras.h"
-#include "Memory/Extras.h"
 
-static TestRunner *Test_Runner = 0;
+#include "Console.h"
+#include "FileSystem/Extras.h"
+#include "Log.h"
+#include "Memory/Extras.h"
+#include "Str.h"
+
+static TestRunner* Test_Runner = 0;
 
 TestCase::TestCase(Str name, Str unit, ProcTest proc)
-    : name(name)
-    , unit(unit)
-    , proc(proc)
+    : name(name), unit(unit), proc(proc)
 {
     get_test_runner()->add_test(*this);
 }
 
 Benchmark::Benchmark(
-    Str name,
-    Str unit,
-    ProcBenchmark *proc,
-    const BenchmarkInputRange &input_range)
-    : name(name)
-    , unit(unit)
-    , proc(proc)
-    , input_range(input_range)
+    Str                        name,
+    Str                        unit,
+    ProcBenchmark*             proc,
+    const BenchmarkInputRange& input_range)
+    : name(name), unit(unit), proc(proc), input_range(input_range)
 {
     get_test_runner()->add_benchmark(*this);
 }
 
-int TestRunner::run_tests() {
-
-    for (TestCase &test_case : test_cases) {
+int TestRunner::run_tests()
+{
+    for (TestCase& test_case : test_cases) {
         Console::set_color(Console::Output, ConsoleColor::Yellow);
         print(LIT("Running "));
         Console::set_color(Console::Output);
@@ -43,7 +39,9 @@ int TestRunner::run_tests() {
             print(LIT("Passed"));
             Console::set_color(Console::Output);
         } else {
-            Console::set_color(Console::Output, ConsoleColor::Red + ConsoleColor::Bold);
+            Console::set_color(
+                Console::Output,
+                ConsoleColor::Red + ConsoleColor::Bold);
             print(LIT("Failed\n\t{}"), test_case.result.reason);
             Console::set_color(Console::Output);
         }
@@ -53,11 +51,12 @@ int TestRunner::run_tests() {
     return 0;
 }
 
-void TestRunner::run_benchmarks() {
-    for (Benchmark &benchmark : benchmarks) {
-
+void TestRunner::run_benchmarks()
+{
+    for (Benchmark& benchmark : benchmarks) {
         print(LIT("Benchmarking "));
-        print(LIT("[\"{}\"] \"{}\" ({}-{}-{}) ... \n"),
+        print(
+            LIT("[\"{}\"] \"{}\" ({}-{}-{}) ... \n"),
             benchmark.unit,
             benchmark.name,
             benchmark.input_range.start,
@@ -65,30 +64,30 @@ void TestRunner::run_benchmarks() {
             benchmark.input_range.end);
 
         for (u64 iteration = benchmark.input_range.start;
-            (iteration <= benchmark.input_range.end);
-            (iteration += benchmark.input_range.step))
+             (iteration <= benchmark.input_range.end);
+             (iteration += benchmark.input_range.step))
         {
             BenchmarkResult result = {};
-            result.iteration = iteration;
-            TimeSpec begin_time = now_time();
+            result.iteration       = iteration;
+            TimeSpec begin_time    = now_time();
             benchmark.proc(result);
             TimeSpec end_time = now_time();
-            float s = (end_time - begin_time).to_s();
+            float    s        = (end_time - begin_time).to_s();
 
-            float units_per_second = (s <= 0.0001f)
-                ? 0.0f
-                : result.units_processed / s;
-            print(LIT("Iteration {} ({} units), {}s {} units/s\n"),
+            float units_per_second =
+                (s <= 0.0001f) ? 0.0f : result.units_processed / s;
+            print(
+                LIT("Iteration {} ({} units), {}s {} units/s\n"),
                 iteration,
                 result.units_processed,
                 s,
                 units_per_second);
         }
-
     }
 }
 
-TestRunner *get_test_runner() {
+TestRunner* get_test_runner()
+{
     if (Test_Runner == 0) {
         Test_Runner = alloc<TestRunner>(System_Allocator, System_Allocator);
     }
