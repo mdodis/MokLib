1c29a2b3aca071616165914dac012238768cef30
Michael Dodis <michaeldodisgr@gmail.com>
2023-03-09 08:24:50 +0200

    refactor Base.h

--- Base.h
+++ Base.h
@@ -1,41 +1,42 @@
 #pragma once
-#include "Host.h"
 #include <stddef.h>
+
+#include "Host.h"
 
 /**
  * Generic Types
  */
 #include <stdint.h>
-typedef uint8_t		uint8;
-typedef uint16_t	uint16;
-typedef uint32_t	uint32;
-typedef uint64_t	uint64;
-typedef int8_t		int8;
-typedef int16_t		int16;
-typedef int32_t		int32;
-typedef int64_t		int64;
-typedef uint8_t*	umm;
+typedef uint8_t  uint8;
+typedef uint16_t uint16;
+typedef uint32_t uint32;
+typedef uint64_t uint64;
+typedef int8_t   int8;
+typedef int16_t  int16;
+typedef int32_t  int32;
+typedef int64_t  int64;
+typedef uint8_t* umm;
 
-typedef uint8_t     u8;
-typedef uint16_t    u16;
-typedef uint32_t    u32;
-typedef uint64_t    u64;
-typedef int8_t      i8;
-typedef int16_t     i16;
-typedef int32_t     i32;
-typedef int64_t     i64;
+typedef uint8_t  u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+typedef int8_t   i8;
+typedef int16_t  i16;
+typedef int32_t  i32;
+typedef int64_t  i64;
 
-typedef float       f32;
-typedef double      f64;
+typedef float  f32;
+typedef double f64;
 
-typedef char*       CStr;
+typedef char* CStr;
 
 #if COMPILER_MSVC
-    #define _inline __forceinline
+#define _inline __forceinline
 #elif COMPILER_GCC || COMPILER_CLANG
-    #define _inline inline __attribute__((always_inline))
+#define _inline inline __attribute__((always_inline))
 #else
-    #error "Unsupported compiler type for _inline!"
+#error "Unsupported compiler type for _inline!"
 #endif
 
 // Signals that is function is not supposed to be used outside of the struct
@@ -45,30 +46,30 @@ typedef char*       CStr;
  * C Compatibility
  */
 #if OS_MSWINDOWS
-    #define DLL_EXPORT __declspec(dllexport)
-    #define DLL_IMPORT __declspec(dllimport)
-    #define CEXPORT extern "C" DLL_EXPORT
+#define DLL_EXPORT __declspec(dllexport)
+#define DLL_IMPORT __declspec(dllimport)
+#define CEXPORT extern "C" DLL_EXPORT
 #endif
 
 #if OS_LINUX
-    #define DLL_EXPORT
-    #define DLL_IMPORT
-    #define CEXPORT extern "C"
+#define DLL_EXPORT
+#define DLL_IMPORT
+#define CEXPORT extern "C"
 #endif
 
 #ifndef CEXPORT
-    #define CEXPORT
+#define CEXPORT
 #endif
 
 /**
  * Miscellaneous
  */
-#define ALIGN_UP2(X, P) ((X) + (P) - 1) & ~((P) - 1)
-#define ALIGN_DOWN2(X, P) ((X) & ~((P) - 1))
+#define ALIGN_UP2(X, P) ((X) + (P)-1) & ~((P)-1)
+#define ALIGN_DOWN2(X, P) ((X) & ~((P)-1))
 
-#define KILOBYTES(num) ((num) * 1024)
-#define MEGABYTES(num) KILOBYTES(((num) * 1024))
-#define GIGABYTES(num) MEGABYTES(((num) * 1024))
+#define KILOBYTES(num) ((num)*1024)
+#define MEGABYTES(num) KILOBYTES(((num)*1024))
+#define GIGABYTES(num) MEGABYTES(((num)*1024))
 
 #define ARRAY_COUNT(a) (sizeof(a) / sizeof((a)[0]))
 
@@ -85,13 +86,13 @@ typedef char*       CStr;
 #define _MSTR(x) #x
 #define MSTR(x) _MSTR(x)
 #define MLITSTR(x) LIT(#x)
-#define OFFSET_OF(type, element) ((size_t)&(((type *)0)->element))
+#define OFFSET_OF(type, element) ((size_t) & (((type*)0)->element))
 
 #define CONTAINER_OF(ptr, type, member) \
-    (type*)(((umm)ptr) - ((u64)OFFSET_OF(type,member)))
+    (type*)(((umm)ptr) - ((u64)OFFSET_OF(type, member)))
 
 #if COMPILER_MSVC
-    #pragma warning(disable : 4251)
+#pragma warning(disable : 4251)
 #endif
 
 /**
@@ -101,10 +102,11 @@ typedef char*       CStr;
  * Beware: this does no checking whatsoever!
  */
 template <typename Target, typename T, typename RetVal, typename... Args>
-Target *as_proc(RetVal(T::*member_proc)(Args...)) {
+Target* as_proc(RetVal (T::*member_proc)(Args...))
+{
     union {
-        RetVal(T::*pf)(Args...);
-        Target *p;
+        RetVal (T::*pf)(Args...);
+        Target* p;
     };
     pf = member_proc;
     return p;
@@ -113,4 +115,7 @@ Target *as_proc(RetVal(T::*member_proc)(Args...)) {
 #define IS_A(what, T) (dynamic_cast<T*>(what) != 0)
 
 template <typename T>
-static _inline T &as(void *ptr) { return *((T*)ptr); }
+static _inline T& as(void* ptr)
+{
+    return *((T*)ptr);
+}
